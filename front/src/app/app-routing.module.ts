d964bbc4d73a5a6eda001bd497c14987f7da44b3
Verrya <63302524+Verrya@users.noreply.github.com>
2024-01-23 16:38:45 +0100

    create router

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AddFilmPageComponent } from './add-film-page/add-film-page.component';
+import { AddScreeningPageComponent } from './add-screening-page/add-screening-page.component';
+import { EditScreeningPageComponent } from './edit-screening-page/edit-screening-page.component';
+import { EditFilmPageComponent } from './edit-film-page/edit-film-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'addFilm', component: AddFilmPageComponent },
+  { path: 'addScreening', component: AddScreeningPageComponent },
+  { path: 'editScreening', component: EditScreeningPageComponent},
+  { path: 'editFilm', component: EditFilmPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

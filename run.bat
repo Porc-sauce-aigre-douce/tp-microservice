b2ba03ae2ba39bd5d717a7e683ffc55071ee980c
Verrya <63302524+Verrya@users.noreply.github.com>
2024-01-23 15:27:35 +0100

    add pnpm i to batch script

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 cd microserviceFilm 
 START mvn spring-boot:run
 cd ../microserviceScreening
+pnpm install
 START node index.js
 cd ../microserviceUsers
+pnpm install
 START node index.js
 cd ../front && ng serve

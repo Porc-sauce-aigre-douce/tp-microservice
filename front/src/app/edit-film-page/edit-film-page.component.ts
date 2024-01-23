7641a769b60022242893e518b604bb6277719206
Verrya <63302524+Verrya@users.noreply.github.com>
2024-01-23 15:43:33 +0100

    transplant code from html to angular

--- front/src/app/edit-film-page/edit-film-page.component.ts
+++ front/src/app/edit-film-page/edit-film-page.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-film-page.component.css']
 })
 export class EditFilmPageComponent {
+  choisirImage() {
+    var inputImage = document.getElementById("inputImage");
+    inputImage.click();
+    
+    // Écouter les changements dans l'input file
+    inputImage.addEventListener("change", function() {
+      afficherImage();
+    });
+  }
 
+  // Fonction pour afficher l'image sélectionnée
+  afficherImage() {
+    var inputImage = document.getElementById("inputImage");
+    var imageAffichee = document.getElementById("imageAffichee");
+
+    // Vérifier s'il y a un fichier sélectionné
+    if (inputImage.files && inputImage.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        imageAffichee.src = e.target.result;
+        imageAffichee.style.display = "block";
+      };
+
+      reader.readAsDataURL(inputImage.files[0]);
+    }
+  }
 }
